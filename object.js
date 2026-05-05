f2c1b740a1d439ada950a11b6bde435e5806be8c
Alok Rajbongshi <alokraj00061@gmail.com>
2026-05-05 19:47:59 +0600

    spaced property name in obj

---
+++ object.js
@@ -0,0 +1,18 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false,
+    married: true,
+    hobbies: ["coding", "hiking"],
+    'fav places': ["Tokyo", "Paris", "New York", "London", "Sydney"]
+}
+
+// dot notation
+console.log(person.age);
+
+// bracket notation 
+
+
+console.log(person['fav places']);
+
