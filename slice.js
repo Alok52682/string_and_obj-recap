228bf79601f04c1de7fe2cb4e0c26a1bf6238edc
Alok Rajbongshi <alokraj00061@gmail.com>
2026-05-05 18:16:48 +0600

    slice and tream recaped

---
+++ slice.js
@@ -0,0 +1,8 @@
+const address = 'andorkilla';
+const part = address.slice(5, 7);
+
+console.log(part);
+
+const sentence = `I am a good and hardworking person.`;
+
+console.log(sentence.split(''))
---
+++ string.js
@@ -0,0 +1,9 @@
+const drink = '   water';
+const liquid = ' water';
+
+if (drink.trim() === liquid.trim()) {
+    console.log('water is life')
+}
+else {
+    console.log('panir opor nam life')
+}
