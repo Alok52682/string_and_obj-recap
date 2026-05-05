442b68dca8cd934d9d76b1f671d89c23e1362c70
Alok Rajbongshi <alokraj00061@gmail.com>
2026-05-05 18:21:41 +0600

    split and join recaped

--- slice.js
+++ slice.js
@@ -5,4 +5,8 @@ console.log(part);
 
 const sentence = `I am a good and hardworking person.`;
 
-console.log(sentence.split(''))
+console.log(sentence.split(''));
+
+const cars = ["Volvo", "BMW", "Ford", "Mazda"];
+
+console.log(cars.join('|'));
